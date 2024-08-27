3c68516c8b342b681a891ab6cd2d4e6b9139306d
Prosexy <166955163+Prosexy@users.noreply.github.com>
2024-08-27 16:46:19 -0700

    Update DemonicHub_V2.lua

--- DemonicHub_V2.lua
+++ DemonicHub_V2.lua
@@ -1,2 +1,2 @@
 local Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/x9PSwiftz/Panda/main/Notification.lua"))()
-Notify.New("Hi Guys, Demonic Hub Is Closed For The Moment, Comeback Tomorrow!",20)
+Notify.New("Hi Guys, Demonic Hub Is Closed For The Moment, Comeback Later!",20)
